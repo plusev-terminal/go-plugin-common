00c50e2d2cce6e12e4f6aab40427c07ce8dca0c1
pk <pk@codeblob.work>
2025-08-28 17:39:25 +0200

    Add meta package

---
+++ meta/meta.go
@@ -0,0 +1,31 @@
+package meta
+
+type Meta struct {
+	PluginID    string          `json:"pluginId"`
+	Name        string          `json:"name"`
+	AppID       string          `json:"appId"`
+	Category    string          `json:"category"` // Plugin category specific to the app id.
+	Description string          `json:"description"`
+	Author      string          `json:"author"`
+	Version     string          `json:"version"`
+	Repository  string          `json:"repository"`
+	Tags        []string        `json:"tags"`
+	Contacts    []AuthorContact `json:"contacts"`
+	Resources   ResourceAccess  `json:"resources"`
+}
+
+type AuthorContact struct {
+	Kind  string `json:"kind"`
+	Value string `json:"value"`
+}
+
+type ResourceAccess struct {
+	AllowedNetworkTargets []NetworkTargetRule `json:"allowedNetworkTargets"`
+	FsWriteAccess         map[string]string   `json:"fsWriteAccess"`
+	StdoutAccess          bool                `json:"stdoutAccess"`
+	StderrAccess          bool                `json:"stderrAccess"`
+}
+
+type NetworkTargetRule struct {
+	Pattern string `json:"pattern"`
+}
