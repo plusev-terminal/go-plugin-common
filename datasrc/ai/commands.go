3484a40eebedd325a11c6ce8ed28755d04a9dd80
pk <pk@codeblob.work>
2026-05-27 20:35:45 +0200

    Add command for AI response streaming

--- datasrc/ai/commands.go
+++ datasrc/ai/commands.go
@@ -1,13 +1,15 @@
 package ai
 
 const (
-	CMD_SEND_PROMPT = "sendPrompt"
-	CMD_GET_MODELS  = "getModels"
+	CMD_SEND_PROMPT        = "sendPrompt"
+	CMD_SEND_PROMPT_STREAM = "sendPromptStream"
+	CMD_GET_MODELS         = "getModels"
 )
 
 func AllCommands() map[string]any {
 	return map[string]any{
-		"CMD_SEND_PROMPT": CMD_SEND_PROMPT,
-		"CMD_GET_MODELS":  CMD_GET_MODELS,
+		"CMD_SEND_PROMPT":        CMD_SEND_PROMPT,
+		"CMD_SEND_PROMPT_STREAM": CMD_SEND_PROMPT_STREAM,
+		"CMD_GET_MODELS":         CMD_GET_MODELS,
 	}
 }
